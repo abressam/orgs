cdec1c5443b5afe47fc693a8b791ac5fcb830fa9
abressam <amandabressam@gmail.com>
2023-04-12 16:05:15 -0300

    Create new activity and update gradle dependency

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
@@ -9,10 +10,9 @@ import com.example.orgs.model.Products
 import com.example.orgs.ui.recyclerview.adapter.ListProductsAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListProductsAdapter(this, products = listOf(
---
+++ app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
@@ -0,0 +1,9 @@
+package com.example.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.orgs.R
+
+class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
+
+}
