5d30a40cb8a89d74a1209ffb482efc935e5f2386
abressam <amandabressam@gmail.com>
2023-04-18 09:17:09 -0300

    Create form

--- app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
@@ -2,8 +2,21 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val saveButton = findViewById<Button>(R.id.save_button)
+        saveButton.setOnClickListener {
+            val nameField = findViewById<EditText>(R.id.name)
+            val name = nameField.text.toString()
+            Log.i("ProductFormActivity", "onCreate: $name")
+        }
+    }
 
 }
