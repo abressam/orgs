dad022fcdeb5c17a33d9d7fedb1aba1420b65be0
abressam <amandabressam@gmail.com>
2023-07-10 21:27:58 -0300

    Get item values

--- app/src/main/java/com/example/orgs/ui/activity/ProductsListActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ProductsListActivity.kt
@@ -2,15 +2,17 @@ package com.example.orgs.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
+import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.orgs.R
 import com.example.orgs.dao.ProductsDao
 import com.example.orgs.databinding.ActivityListProductsBinding
+import com.example.orgs.databinding.ActivityProductFormFieldsBinding
+import com.example.orgs.ui.dialog.FormItemListDialog
 import com.example.orgs.ui.recyclerview.adapter.ListProductsAdapter
-import com.google.android.material.textfield.TextInputEditText
+import java.math.BigDecimal
 
 class ProductsListActivity : AppCompatActivity(R.layout.activity_list_products) {
 
@@ -49,14 +51,13 @@ class ProductsListActivity : AppCompatActivity(R.layout.activity_list_products) 
 
         adapter.setOnItemClickListener(object : ListProductsAdapter.RecyclerViewInterface {
             override fun onItemClick(position: Int) {
-                Toast.makeText(this@ProductsListActivity, "You clicked in $position!", Toast.LENGTH_SHORT).show()
-                AlertDialog.Builder(this@ProductsListActivity)
-                    .setMessage("Altere os dados do produto como desejar")
-                    .setTitle("Produto selecionado")
-                    .setView(R.layout.activity_product_form_fields)
-                    .setPositiveButton("Salvar") { _, _ -> }
-                    .setNegativeButton("Cancelar") { _, _ -> }
-                    .show()
+                val item = adapter.getItem(position)
+                val title = item.title_product
+                val description = item.description
+                val price = item.price.toString()
+
+                FormItemListDialog(this@ProductsListActivity, adapter)
+                    .showItemListDialog(title, description, price, position)
             }
         })
     }
