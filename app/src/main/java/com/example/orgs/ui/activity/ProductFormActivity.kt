09cb7a4b832b3e96ca58bb8bd23975937b765401
abressam <amandabressam@gmail.com>
2023-07-10 21:23:40 -0300

    Implements upload image and error message

--- app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/ProductFormActivity.kt
@@ -1,34 +1,58 @@
 package com.example.orgs.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import com.example.orgs.R
 import com.example.orgs.dao.ProductsDao
 import com.example.orgs.databinding.ActivityProductFormBinding
+import com.example.orgs.extensions.tryLoadImage
 import com.example.orgs.model.Products
+import com.example.orgs.ui.dialog.FormImageDialog
 import java.math.BigDecimal
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
 
     private lateinit var binding: ActivityProductFormBinding
     private val dao = ProductsDao()
+    private var url: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProductFormBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        title = "Cadastrar produto"
         configSaveButton()
+//        enableSaveButton()
         configCancelButton()
 
         binding.activityProductImage.setOnClickListener {
-            AlertDialog.Builder(this)
-                .setView(R.layout.activity_product_form_image)
-                .setPositiveButton("Carregar") { _, _ -> }
-                .setNegativeButton("Cancelar") { _, _ -> }
-                .show()
+            FormImageDialog(this)
+                .showDialog(url) { image ->
+                url = image
+                binding.activityProductImage.tryLoadImage(url)
+                }
         }
+
+        errorMessage()
+    }
+
+    private fun errorMessage() {
+        val title_product = binding.activityProductFormTitleProduct.text.toString()
+
+        binding.activityProductFormTitleProduct.setOnFocusChangeListener { _, focused ->
+            if(!focused && title_product.isEmpty())
+                binding.activityProductFormTextInputTitle.error = "Campo não pode ser vazio"
+            else
+                binding.activityProductFormTextInputTitle.error = null
+
+        }
+//        binding.activityProductFormTitleProduct.doOnTextChanged { text, start, before, count ->
+//            if(text!!.isNotEmpty())
+//                binding.activityProductFormTextInputTitle.error = null
+//            else
+//                binding.activityProductFormTextInputTitle.error = "Campo não pode ser vazio"
+//        }
     }
 
     private fun configCancelButton() {
@@ -40,17 +64,13 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
     private fun configSaveButton() {
         binding.activityProductFormSaveButton.setOnClickListener {
             val newProduct = createProduct()
-
-            Log.i("ProductFormActivity", "onCreate: $newProduct")
-
             dao.addProduct(newProduct)
-            Log.i("ProductFormActivity", "onCreate: ${dao.listAll()}")
             finish()
         }
     }
 
     private fun createProduct(): Products {
-        val name = binding.activityProductFormTitleProduct.text.toString()
+        val title_product = binding.activityProductFormTitleProduct.text.toString()
         val description = binding.activityProductFormDescription.text.toString()
         val priceText = binding.activityProductFormPrice.text.toString()
 
@@ -61,9 +81,10 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
         }
 
         return Products(
-            title_product = name,
+            title_product = title_product,
             description = description,
-            price = price
+            price = price,
+            image = url
         )
     }
 
