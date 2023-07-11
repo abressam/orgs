7031064572e3bd5adeb71ea597e40728ccb4a36f
abressam <amandabressam@gmail.com>
2023-07-10 21:26:42 -0300

    Add load function

--- app/src/main/java/com/example/orgs/ui/recyclerview/adapter/ListProductsAdapter.kt
+++ app/src/main/java/com/example/orgs/ui/recyclerview/adapter/ListProductsAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.orgs.ui.recyclerview.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.orgs.R
 import com.example.orgs.databinding.ProductsItemBinding
+import com.example.orgs.extensions.tryLoadImage
 import com.example.orgs.model.Products
+import java.math.BigDecimal
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -30,7 +34,16 @@ class ListProductsAdapter(
             binding.productItemDescription.text = products.description
             val brasilianCoinFormat : String = brasilianCoin(products)
             binding.productItemPrice.text = brasilianCoinFormat
-            binding.productItemImage.load("https://images.pexels.com/photos/1055272/pexels-photo-1055272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2")
+
+            val visible = if(products.image != null) {
+                View.VISIBLE
+            } else {
+                View.GONE // não mostra o conteúdo
+            }
+
+            binding.productItemImage.visibility = visible
+
+            binding.productItemImage.tryLoadImage(products.image)
         }
 
         private fun brasilianCoin(product: Products): String {
@@ -64,6 +77,22 @@ class ListProductsAdapter(
         this.products.clear()
         this.products.addAll(products)
         notifyDataSetChanged()
+    }
+
+    fun delete(position: Int) {
+        this.products.removeAt(position)
+        notifyDataSetChanged()
+    }
+
+    fun updateItem(position: Int, title: String, description: String, price: BigDecimal) {
+        products[position].title_product = title
+        products[position].description = description
+        products[position].price = price
+        notifyDataSetChanged()
+    }
+
+    fun getItem(position: Int): Products {
+        return products[position]
     }
 
 }
